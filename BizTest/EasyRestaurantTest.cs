7cd1e05e47e4abee4c55a74a65bc7e3b7c92acc0
JasonJDong <amycolin@126.com>
2013-04-18 22:27:49 +0800

    增加单元测试，修改自动赋值基类属性大小写敏感的BUG

---
+++ BizTest/EasyRestaurantTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using EasyRestaurantBusiness;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BizTest
+{
+    [TestClass]
+    public class EasyRestaurantTest
+    {
+        private EasyRestaurant easyRestaurant = new EasyRestaurant();
+        [TestMethod]
+        public void TestLoginForSession()
+        {
+            var user = easyRestaurant.LoginForSession("dong", "12345");
+            Assert.IsNotNull(user.Session);
+        }
+    }
+}
